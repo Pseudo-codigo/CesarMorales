9db340efd4580436328eff92d0a7bc2c87321197
Carlos Morales <carlos.morales@cit.hn>
2020-08-14 11:55:06 -0600

    -Added Sidebar
    Added Multiple components structure,
    Added sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,148 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContainerComponent } from './layout/container/container.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: ContainerComponent,
+    // children: [
+    //   {
+    //     path:'Home',
+    //     redirectTo: ''
+    //   },
+    //   {
+    //     path: '',
+    //     component: HomeComponent,
+    //     children: [
+    //       {
+    //         path: '',
+    //         component: VideoContainerComponent
+    //       },
+    //       {
+    //         path: 'Articles',
+    //         component: ArticleContainerComponent,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'About',
+    //     component: AboutComponent,
+    //   },
+    //   {
+    //     path: 'Library',
+    //     component: LibraryComponent,
+    //     children: [
+    //       {
+    //         path: '',
+    //         redirectTo: 'Videos',
+    //         pathMatch: 'full'
+    //       },
+    //       {
+    //         path: 'Videos',
+    //         component: VideoContainerComponent,
+    //       },
+    //       {
+    //         path: 'Articles',
+    //         component: ArticleContainerComponent,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     path: 'News',
+    //     component: NewsComponent,
+    //   },
+    //   {
+    //     path: 'Cart',
+    //     component: CartComponent,
+    //   },
+    //   {
+    //     path: 'OrderSummary/:id',
+    //     component: SummaryComponent,
+    //     canActivate: [AuthGuard]
+    //   },
+    //   {
+    //     path: 'Checkout',
+    //     component: CheckoutComponent,
+    //     canActivate: [AuthGuard],
+    //   },
+    //   {
+    //     path: 'Shop',
+    //     children: [
+    //       {
+    //         path: '',
+    //         component: ShopCategoryComponent
+    //       },
+    //       {
+    //         path: 'Category',
+    //         redirectTo: '',
+    //       },
+    //       {
+    //         path: 'Category/:Category',
+    //         component: ShopComponent
+    //       },
+    //       {
+    //         path: 'Detail',
+    //         redirectTo: '',
+    //       },
+    //       {
+    //         path: 'Detail/:Detail',
+    //         component: ShopItemDetailComponent
+    //       },
+    //     ]
+    //   },
+    //   {
+    //     path: 'Gallery',
+    //     component: GalleryComponent,
+    //   },
+    //   {
+    //     path: 'Register',
+    //     children: [
+    //       { path: '', component: RegisterComponent, data: { animation: 'Register' } },
+    //       { path: 'PlanForm', component: PlanFormComponent, data: { animation: 'PlanForm' } },
+    //       { path: 'Registration', component: RegistrationComponent, data: { animation: 'Registration' } },
+    //       { path: 'RegForm', component: RegFormComponent, data: { animation: 'RegForm' } },
+    //       { path: 'Payment', component: PaymentComponent, data: { animation: 'Payment' } },
+    //       { path: 'CreditOption', component: CreditOptionComponent, data: { animation: 'CreditOption' } },
+    //     ]
+    //   },
+    //   {
+    //     path: 'Dashboard',
+    //     component: DashboardComponent,
+    //     canActivate: [AuthGuard]
+    //   },
+    //   {
+    //     path: 'VerifyEmail',
+    //     component: VerifyEmailComponent
+    //   },
+    //   {
+    //     path: 'ResetPassword',
+    //     component: ResetPasswordComponent
+    //   },
+    //   {
+    //     path: '',
+    //     component: HomeComponent,
+    //   },
+    // ]
+  },
+  // {
+  //   path: 'Welcome',
+  //   component: ContainerComponent,
+  //   children: [
+  //     {
+  //       path: '',
+  //       component: WelcomeComponent,
+  //     },
+  //   ]
+  // },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { ContainerComponent } from './layout/container/container.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContainerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
