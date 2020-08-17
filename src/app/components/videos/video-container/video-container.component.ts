c79aa7e4e214fbcabd70499bcdd42748dbc7e3f8
Carlos Morales <carlos.morales@cit.hn>
2020-08-17 01:26:30 -0600

    Added Services
    -Added Sevices Section
    -Made videos dynamic

--- src/app/components/videos/video-container/video-container.component.ts
+++ src/app/components/videos/video-container/video-container.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-video-container',
   templateUrl: './video-container.component.html',
   styleUrls: ['./video-container.component.scss']
 })
 export class VideoContainerComponent implements OnInit {
+  Videos = [];
+
+  Vids = [
+    {
+      Title: 'VIDEOS RECIENTES 2018',
+      Text: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPI- SCING ELIT, SED DIAM NONUMMY NIBH EUISMOD TINCID- UNT UT LAOREET DOLORE MAGNA ALIQUAM ERAT VOLUT- PAT. UT WISI',
+      Subtext: 'LOREM IPSUM DOLOR SIT AMET',
+      Link: 'https://google.com',
+      Image: '../../../../assets/img/SVGs/colors/purple.svg',
+    },
+    {
+      Title: 'VIDEOS RECIENTES 2019',
+      Text: 'LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPI- SCING ELIT, SED DIAM NONUMMY NIBH EUISMOD TINCID- UNT UT LAOREET DOLORE MAGNA ALIQUAM ERAT VOLUT- PAT. UT WISI',
+      Subtext: 'LOREM IPSUM DOLOR SIT AMET',
+      Link: 'https://google.com',
+      Image: '../../../../assets/img/SVGs/colors/green.svg',
+    },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.GetVideos();
+    console.log(this.Videos)
+  }
+
+  GetVideos() {
+
+    this.Vids.map((video, index) => {
+      let first: boolean = false;
+      let last: boolean = false;
+      let left: boolean = false;
+      let right: boolean = false;
+
+      if (index === 0) {
+        first = true
+      }
+      else if (index === this.Vids.length - 1) {
+        last = true
+      }
+
+      if (index % 2) {
+        left = true
+      }
+      else {
+        right = true
+      }
+
+      let videoTemp = {
+        First: first,
+        Last: last,
+        Left: left,
+        Right: right,
+        Image: video.Image,
+        Link: video.Link,
+        Title: video.Title,
+        Text: video.Text,
+        Subtext: video.Subtext,
+      }
+
+      this.Videos.push(videoTemp);
+
+    });
   }
 
 }
--- src/app/components/videos/videos/videos.component.ts
+++ src/app/components/videos/videos/videos.component.ts
@@ -7,16 +7,46 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class VideosComponent implements OnInit {
   @Input("First") First: boolean = false;
+  @Input("Last") Last: boolean = false;
   @Input("Left") Left: boolean = false;
   @Input("Right") Right: boolean = false;
   @Input("Image") Image: string = '';
   @Input("Link") Link: string = '';
+  @Input("Title") Title: string = '';
   @Input("Text") Text: string = '';
+  @Input("Subtext") Subtext: string = '';
   @Input("ButtonText") ButtonText: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.Right && this.Left) {
+      this.Right = false;
+    }
+
+    this.getSource();
+  }
+
+  getClasses(type: number): string {
+    let classes = 'col-12 col-md-6 ';
+
+    switch (type) {
+      case 1:
+        classes += 'p-0 bg-primary';
+        classes += this.Left ? 'order-last order-md-first BorderRight' : 'order-last BorderLeft'
+        break;
+      case 2:
+        classes += 'p-4 m-auto ';
+        classes += this.Left ? 'order-first order-md-last' : 'order-first'
+        break;
+    }
+    return classes;
+  }
+
+  getSource = () => {
+    if (this.Image == '' || !this.Image) {
+      this.Image = '../../../../assets/img/SVGs/colors/yellow.svg'
+    }
   }
 
 }
